fcc9903a9665bdb29789c365bc50c90497bf58bd
ZXC-zxc <xiaochen@ftsafe.com>
2020-07-14 23:11:55 -0700

    add consmos tx sign

--- test/test.ts
+++ test/test.ts
@@ -1,22 +1,23 @@
 debugger;
 
-import {JubDID,KeyPair,JubRegistry,JubResolver,createHDKeyPair} from '../src/index';
+import { JubDID, KeyPair, JubRegistry, JubResolver, createHDKeyPair } from '../src/index';
 
 
 async function testDID() {
     //create
-    const MNEMONIC = "gauge hole clog property soccer idea cycle stadium utility slice hold chief";
-    let key:KeyPair = createHDKeyPair(MNEMONIC,'44\'/364\'/0\'/0/0');
+    const MNEMONIC = "wolf thumb intact fantasy cave lonely barely setup dress life invest kingdom potato apple iron sentence sense paddle then ability minimum attract pottery glue";
+    let key: KeyPair = createHDKeyPair(MNEMONIC, '44\'/118\'/0\'/0/0');
     let jubDID = new JubDID(key);
     console.log(jubDID.keyPair.sk);
     console.log(jubDID.keyPair.pk);
     console.log(jubDID.getSubject());
+
     //registry
     let registry = new JubRegistry("http://did.jubiterwallet.com/registry");
     let rv = await registry.registry(jubDID);
     //change owner
-    let newKey = createHDKeyPair(MNEMONIC,'44\'/364\'/0\'/0/1');
-    rv = await registry.changeOwner(jubDID,newKey);
+    let newKey = createHDKeyPair(MNEMONIC, '44\'/118\'/0\'/0/1');
+    rv = await registry.changeOwner(jubDID, newKey);
 
     //sign Jwt
 
