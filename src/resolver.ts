83d0913a80797a60c611cf1e8e03e3292b72d8f6
changliang0931 <changliang0931@126.com>
2020-09-29 18:48:19 +0800

    实现Resolvable

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,13 +1,20 @@
-import {fetchGetData} from "./fetchUtil"
+import { fetchGetData } from "./fetchUtil"
+import {Resolvable} from "did-jwt"
 
-export class JubResolver{
+export class JubResolver implements Resolvable{
     url:string;
     constructor(url:string){
         this.url = url;
     }
 
-    async resolve(didSubject:string):Promise<string>{
+    async resolve(didSubject:string):Promise <any>{
         const url = this.url + "/1.0/identifiers/" + didSubject;
-        return fetchGetData(url);
+        return new Promise((resolve, reject) => {
+            fetchGetData(url).then(function (ret) {
+                    return resolve(ret.didDocument);
+                }).catch(function (error) {
+                    return reject(error);
+                });
+            }); 
     }
 }
