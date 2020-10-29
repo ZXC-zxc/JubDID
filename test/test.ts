19c1f812aa18c16b85b856dc88a93f9c36301737
changliang0931 <changliang0931@126.com>
2020-10-29 15:46:53 +0800

    测试

--- test/test.ts
+++ test/test.ts
@@ -1,53 +1,74 @@
 debugger;
 
-import { JubDID, KeyPair, JubRegistry, JubResolver, createHDKeyPair } from '../src/index';
+import { JubDID, KeyPair, JubRegistry, JubResolver, createHDKeyPair,generateRandomMnemonic ,verifyCredential} from '../src/index';
 import { FtsafeProxy } from '../src/FtsafeProxy';
 
+var response: any;
+const FTURL = "http://39.102.40.227/registrar";
+const REGURL = "http://39.102.40.227/registrar";
+const COSMOSURL = "http://39.102.40.227/blockchain";
+const RESOLVEURL = "http://39.102.40.227/resolver";
+const FTSAFE_PROXY  = new FtsafeProxy(FTURL);
+const REGISTRY  = new JubRegistry(REGURL,COSMOSURL);
+const RESOLVER = new JubResolver(RESOLVEURL);
 
-async function testDID() {
-    // step 1 : Create Key Pair
-    const MNEMONIC = "gauge hole clog property soccer idea cycle stadium utility slice hold chief";
-    const key: KeyPair = createHDKeyPair(MNEMONIC, '44\'/118\'/0\'/0/0');
-    console.log(key.sk);
-    console.log(key.pk);
-    console.log(key.address);
+// Create Key Pair
+// console.log("generateRandomMnemonic:"+generateRandomMnemonic(128));
+const MNEMONIC = "mind crop lazy bid wing eager wash mountain trick galaxy earth broken";
+const KEY_DEFAULT: KeyPair = createHDKeyPair(MNEMONIC, '44\'/118\'/0\'/0/0');
+console.log("default key [address]: "+KEY_DEFAULT.address);
+console.log("default key [pk_bech32]: "+KEY_DEFAULT.pk_bech32);
+const JUB_DID = new JubDID(KEY_DEFAULT);
 
-    // step 2 : Use ftsafe proxy to create a new cosmos account
-    const FTURL = "http://39.102.46.62:9080";
-    // let ftsafeProxy  = new FtsafeProxy(FTURL);
-    // let response = await ftsafeProxy.createAccount(key.address,key.pk_bech32);
-
-    // setp 3 : Use DID registry to regist a DID
-    const REGURL = "http://39.102.46.62:9080";
-    const COSMOSURL = "http://39.102.46.62:1317";
-    const jubDID = new JubDID(key);
-    const registry = new JubRegistry(REGURL,COSMOSURL);
-    const response = await registry.signvc(jubDID);
-    // var response = await registry.registry(jubDID);
-    // console.log(response);
-    // step 4 : resolve DID
-    const RESOLVEURL = "http://39.102.46.62:8080";
-    const resolver = new JubResolver(RESOLVEURL);
-    const oldDocument = await resolver.resolve(jubDID.getSubject());
-    console.log("oldDocument:"+JSON.stringify(oldDocument));
-    // step 5 : update DID
-    // let updateKeyPair: KeyPair = createHDKeyPair(MNEMONIC, '44\'/118\'/0\'/0/1');
-    // console.log("updateKeyPair: "+updateKeyPair.sk);
-    // console.log("updateKeyPair: "+updateKeyPair.pk);
-    // console.log("updateKeyPair: "+updateKeyPair.address);
-    // console.log("updateKeyPair: "+updateKeyPair.pk_bech32);
-    // var response = await registry.update(jubDID, updateKeyPair);
-    // console.log(response);
-    // var newDocument = await resolver.resolve(jubDID.getSubject());
-    // console.log("newDocument:"+JSON.stringify(newDocument));
-    // step 6 : sign JWT
-    // step 7 : verify JWT
-    // step 8 : deactivate DID
-    // var response = await registry.deactivate(jubDID);
-    // console.log(response);
-    const deactivateDocument = await resolver.resolve(jubDID.getSubject());
-    console.log("deactivateDocument:"+JSON.stringify(deactivateDocument));
-
+{
+    // createAccount();
+    // registryDID();
+    // resolveDID();
+    // signJWT();
+    // verifyJWT();
+}
+// Use ftsafe proxy to create a new cosmos account
+async function createAccount() {
+    response = await FTSAFE_PROXY.createAccount(KEY_DEFAULT.address, KEY_DEFAULT.pk_bech32);
+    console.log("createAccount:"+JSON.stringify(response));
 };
-
-testDID();
+// Use DID registry to regist a DID
+async function registryDID() {
+    response = await REGISTRY.registry(JUB_DID);
+    console.log("registryDID:"+JSON.stringify(response));
+};
+// resolve DID
+async function resolveDID() {
+    const oldDocument = await RESOLVER.resolve(JUB_DID.getSubject());
+    console.log("resolveDID:"+JSON.stringify(oldDocument));
+};
+// sign JWT
+async function signJWT() {
+    const signedVC = await REGISTRY.signvc(JUB_DID);
+    console.log("signJWT:"+signedVC);
+};
+// verify JWT
+async function verifyJWT() {
+    const signedVC = await REGISTRY.signvc(JUB_DID)
+    const verifyJWTInfo = await verifyCredential(signedVC, RESOLVER);
+    console.log("verifyJWT:"+JSON.stringify(verifyJWTInfo));
+};
+//  update DID
+async function updateDID() {
+    const mnemonic = generateRandomMnemonic(128);
+    console.log("updateDID[mnemonic]: "+ mnemonic);
+    let updateKeyPair: KeyPair = createHDKeyPair(mnemonic, '44\'/118\'/0\'/0/1');
+    console.log("updateKeyPair[address]: "+updateKeyPair.address);
+    console.log("updateKeyPair[pk_bech32]: "+updateKeyPair.pk_bech32);
+    response = await REGISTRY.update(JUB_DID, updateKeyPair);
+    console.log("updateDID"+JSON.stringify(response));
+    var newDocument = await RESOLVER.resolve(JUB_DID.getSubject());
+    console.log("newDocument:"+JSON.stringify(newDocument));
+};
+// // deactivate DID
+async function deactivateDID() {
+    response = await REGISTRY.deactivate(JUB_DID);
+    console.log("deactivateDID response"+JSON.stringify(response));
+    const deactivateDocument = await RESOLVER.resolve(JUB_DID.getSubject());
+    console.log("deactivateDID :"+JSON.stringify(deactivateDocument));
+};
