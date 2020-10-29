d03f01c93bb8a726e0f530f8c79b98db4cf8799c
changliang0931 <changliang0931@126.com>
2020-10-29 15:45:54 +0800

    增加schema方法

--- src/JubDID.ts
+++ src/JubDID.ts
@@ -11,7 +11,6 @@ export type KeyPair = {
     pk_bech32: string,
 };
 
-
 export function createHDKeyPair(Mnemoic: string, Path: string): KeyPair {
     const keys = cosmos.crypto.getKeysFromMnemonic(Mnemoic, Path);
     const key: KeyPair = {
@@ -22,6 +21,10 @@ export function createHDKeyPair(Mnemoic: string, Path: string): KeyPair {
     }
 
     return key;
+}
+
+export function generateRandomMnemonic (params: number): string {
+    return cosmos.crypto.generateRandomMnemonic(params |128 );
 }
 
 export class JubDID {
@@ -61,7 +64,7 @@ export class JubDID {
         const json: any = {
             "authentication": [
                 {
-                    "type": "Secp256k1",
+                    "type": "Secp256k1SignatureAuthentication2018",
                     "publicKey": [
                         subject + "#key-1"
                     ]
@@ -70,7 +73,7 @@ export class JubDID {
             "publicKey": [
                 {
                     "id": subject + "#key-1",
-                    "type": "Secp256k1",
+                    "type": "Secp256k1VerificationKey2018",
                     "publicKeyHex": newKeyPair.pk
                 }
             ]
@@ -79,22 +82,23 @@ export class JubDID {
         return JSON.stringify(json);
     }
 
-    getVcPayload(): JwtCredentialPayload {
-        const payload: JwtCredentialPayload = {
-            sub: this.getSubject(),
-            nbf: 1562950282,
-            vc: {
-                '@context': ['https://www.w3.org/2018/credentials/v1'],
-                type: ['VerifiableCredential'],
-                credentialSubject: {
-                    degree: {
-                        type: 'BachelorDegree',
-                        name: 'Baccalauréat en musiques numériques'
-                    }
+    getVcPayloadSchema(schema?: any): JwtCredentialPayload {
+        const vc = schema || {
+            '@context': ['https://www.w3.org/2018/credentials/v1'],
+            type: ['VerifiableCredential'],
+            credentialSubject: {
+                degree: {
+                    type: 'BachelorDegree',
+                    name: 'Baccalauréat en musiques numériques'
                 }
             }
         };
-
+        const payload: JwtCredentialPayload = {
+            sub: this.getSubject(),
+            nbf: 1562950282,
+            vc: vc
+        };
+        
         return payload
     }
 }
