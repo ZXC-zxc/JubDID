14a57f25e9767a1d60603eb621b237343af23c71
changliang0931 <changliang0931@126.com>
2020-10-29 15:46:17 +0800

    增加随机助剂码方法

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {JubDID, KeyPair, createHDKeyPair} from "./JubDID";
+import {JubDID, KeyPair, createHDKeyPair,generateRandomMnemonic} from "./JubDID";
 import {JubRegistry} from "./registry";
 import {JubResolver} from "./resolver";
 import {verifyCredential} from "did-jwt-vc";
 
 
-export {JubDID,KeyPair,JubRegistry,JubResolver,createHDKeyPair, verifyCredential};
+export {JubDID,KeyPair,JubRegistry,JubResolver,createHDKeyPair,generateRandomMnemonic, verifyCredential};
